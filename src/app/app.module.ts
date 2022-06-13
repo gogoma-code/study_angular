9709eb4880ca2248c9b01af7e8ffa8f1a3d70e87
lyk-brave <officeyongki@gmail.com>
2022-06-13 09:50:09 +0900

    angular init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule
